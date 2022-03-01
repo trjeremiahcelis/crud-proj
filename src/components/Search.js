40689dbb190d3b119a9ea344fb57cf2b6a2a8e47
trjeremiahcelis <jeremiah.celis@tooltwist.com>
2022-03-01 15:34:39 +0800

    commit on dev

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 const Search = (props) => {
     const handleChange = (e) => { setSearchTerm(e.target.value) };
     const searchTerm = props.searchTerm;
