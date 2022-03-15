3ebca9322d636b55cd6cc67d4fa35c58fd9371b6
trjeremiahcelis <98722252+trjeremiahcelis@users.noreply.github.com>
2022-03-15 11:27:45 +0800

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Crud from './pages/Crud';
 import Home from './pages/Home';
@@ -11,7 +11,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/Table" element={<Crud />} />
         </Routes>
-        {/* <Crud /> */}
       </div>
   );
 }
