8d882d31c1ea882801ca2cf50abcfd8e4a6e6b88
trjeremiahcelis <98722252+trjeremiahcelis@users.noreply.github.com>
2022-03-15 11:57:40 +0800

    change import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/styles/app.scss';
+import './styles/app.scss';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
