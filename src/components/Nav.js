da46899abf7b1151271cd09e8652db5b900d45af
trjeremiahcelis <98722252+trjeremiahcelis@users.noreply.github.com>
2022-03-18 17:28:48 +0800

    updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import Icon from '@mdi/react'
-import { mdiTableLarge, mdiHome } from '@mdi/js'
+import { mdiAccountGroup , mdiHome } from '@mdi/js'
 import Logo from '../assets/images/Logo-01.svg'
 
 const Nav = () => {
@@ -8,17 +8,17 @@ const Nav = () => {
             <ul className="before">
                 <li><img src={Logo} alt="logo" /></li>
                 <li><a href="/">
-                    <Icon path={mdiHome} title="action" size={1} className='Table' color='white' />
+                    <Icon path={mdiHome} title="action" size={1} className='Table' color='grey' />
                 </a></li>
                 <li><a href="/Table">
-                    <Icon path={mdiTableLarge} title="action" size={0.9} className='Table' color='white' />
+                    <Icon path={mdiAccountGroup } title="action" size={0.9} className='Table' color='grey' />
                 </a></li>
             </ul>
 
             <ul className="after">
                 <li><img src={Logo} alt="logo" /></li>
                 <li><a href="/">Home</a></li>
-                <li><a href="/Table">Table</a></li>
+                <li><a href="/Students">Students</a></li>
             </ul>
         </nav>
     );
